e14b29f6ecec9da6d13be035cf7e1effccc99047
大鱼 <clygd@126.com>
2018-03-17 23:37:40 +0800

    player

--- ABigFishTV/AppDelegate.m
+++ ABigFishTV/AppDelegate.m
@@ -33,7 +33,7 @@
 
 #import "XHLaunchAd.h"
 
-@interface AppDelegate ()<UNUserNotificationCenterDelegate>
+@interface AppDelegate ()<UNUserNotificationCenterDelegate,XHLaunchAdDelegate>
 
 @end
 
@@ -59,119 +59,24 @@
     
     
     
+    [self initShareSdk];
+    [self initUM:launchOptions];
     
-    //设置你工程的启动页使用的是:LaunchImage 还是 LaunchScreen.storyboard(不设置默认:LaunchImage)
-    [XHLaunchAd setLaunchSourceType:SourceTypeLaunchImage];
     
-    //配置广告数据
-    XHLaunchImageAdConfiguration *imageAdconfiguration = [XHLaunchImageAdConfiguration new];
-    //广告停留时间
-    imageAdconfiguration.duration = 5;
-    //广告frame
-    imageAdconfiguration.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-130);
-    //广告图片URLString/或本地图片名(.jpg/.gif请带上后缀)
-    imageAdconfiguration.imageNameOrURLString = @"image12.gif";
-    //设置GIF动图是否只循环播放一次(仅对动图设置有效)
-    imageAdconfiguration.GIFImageCycleOnce = YES;
-    //网络图片缓存机制(只对网络图片有效)
-    imageAdconfiguration.imageOption = XHLaunchAdImageRefreshCached;
-    //图片填充模式
-    imageAdconfiguration.contentMode = UIViewContentModeScaleToFill;
-    //广告点击打开页面参数(openModel可为NSString,模型,字典等任意类型)
-    imageAdconfiguration.openModel = @"http://abigfish.org";
-    //广告显示完成动画
-    imageAdconfiguration.showFinishAnimate =ShowFinishAnimateFadein;
-    //广告显示完成动画时间
-    imageAdconfiguration.showFinishAnimateTime = 0.8;
-    //跳过按钮类型
-    imageAdconfiguration.skipButtonType = SkipTypeTimeText;
-    //后台返回时,是否显示广告
-    imageAdconfiguration.showEnterForeground = NO;
-    //设置要添加的子视图(可选)
-    //imageAdconfiguration.subViews = ...
-    //显示图片开屏广告
-    [XHLaunchAd imageAdWithImageAdConfiguration:imageAdconfiguration delegate:self];
-    
-    [NSThread sleepForTimeInterval:2.0];
+    //[NSThread sleepForTimeInterval:2.0];
     
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     
     self.window.rootViewController = [[ABFTabBarController alloc]init];
     
+    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidFinishLaunchingNotification object:nil queue:nil usingBlock:^(NSNotification * _Nonnull note) {
+        //初始化开屏广告
+        [self setupXHLaunchAd];
+    }];
+    
     [self.window makeKeyAndVisible];
     
     [self netWorkChangeEvent];
-    
-    
-    [ShareSDK registerActivePlatforms:@[
-                                        @(SSDKPlatformTypeSinaWeibo),
-                                        @(SSDKPlatformTypeMail),
-                                        @(SSDKPlatformTypeCopy),
-                                        @(SSDKPlatformTypeWechat),
-                                        @(SSDKPlatformTypeQQ)                                        ]
-                             onImport:^(SSDKPlatformType platformType)
-     {
-         switch (platformType)
-         {
-             case SSDKPlatformTypeWechat:
-                 [ShareSDKConnector connectWeChat:[WXApi class]];
-                 break;
-             case SSDKPlatformTypeQQ:
-                 [ShareSDKConnector connectQQ:[QQApiInterface class] tencentOAuthClass:[TencentOAuth class]];
-                 break;
-             case SSDKPlatformTypeSinaWeibo:
-                 [ShareSDKConnector connectWeibo:[WeiboSDK class]];
-                 break;
-             default:
-                 break;
-         }
-     }
-                      onConfiguration:^(SSDKPlatformType platformType, NSMutableDictionary *appInfo)
-     {
-         
-         switch (platformType)
-         {
-             case SSDKPlatformTypeSinaWeibo:
-                 //设置新浪微博应用信息,其中authType设置为使用SSO＋Web形式授权
-                 [appInfo SSDKSetupSinaWeiboByAppKey:@"3780891600"
-                                           appSecret:@"9dd622fbca8d2ce9b90ce7fe63f4c16c"
-                                         redirectUri:@"http://www.abigfish.org"
-                                            authType:SSDKAuthTypeBoth];
-                 break;
-             case SSDKPlatformTypeWechat:
-                 [appInfo SSDKSetupWeChatByAppId:@"wx4868b35061f87885"
-                                       appSecret:@"64020361b8ec4c99936c0e3999a9f249"];
-                 break;
-             case SSDKPlatformTypeQQ:
-                 [appInfo SSDKSetupQQByAppId:@"1106573647"
-                                      appKey:@"PkUjxVUWwIX7cUiS"
-                                    authType:SSDKAuthTypeBoth];
-                 break;
-             default:
-                 break;
-         }
-     }];
-    
-    
-    [UMConfigure initWithAppkey:@"5a9fef97f29d984356000310" channel:@"App Store"];
-    //[UMErrorCatch initErrorCatch];
-    
-    [MobClick setScenarioType:E_UM_NORMAL];
-    
-    // Push组件基本功能配置
-    [UNUserNotificationCenter currentNotificationCenter].delegate = self;
-    UMessageRegisterEntity * entity = [[UMessageRegisterEntity alloc] init];
-    //type是对推送的几个参数的选择，可以选择一个或者多个。默认是三个全部打开，即：声音，弹窗，角标等
-    entity.types = UMessageAuthorizationOptionBadge|UMessageAuthorizationOptionAlert;
-    [UNUserNotificationCenter currentNotificationCenter].delegate = self;
-    [UMessage registerForRemoteNotificationsWithLaunchOptions:launchOptions Entity:entity completionHandler:^(BOOL granted, NSError * _Nullable error) {
-        if (granted) {
-            // 用户选择了接收Push消息
-        }else{
-            // 用户拒绝接收Push消息
-        }
-    }];
-    
     
     
     //[NSThread sleepForTimeInterval:2.0];
@@ -254,6 +159,140 @@
     NSLog(@"The response of wechat.");
 }
 
+-(void)initUM:(NSDictionary *)launchOptions{
 
+    [UMConfigure initWithAppkey:@"5a9fef97f29d984356000310" channel:@"App Store"];
+    //[UMErrorCatch initErrorCatch];
+    
+    [MobClick setScenarioType:E_UM_NORMAL];
+    
+    // Push组件基本功能配置
+    [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+    UMessageRegisterEntity * entity = [[UMessageRegisterEntity alloc] init];
+    //type是对推送的几个参数的选择，可以选择一个或者多个。默认是三个全部打开，即：声音，弹窗，角标等
+    entity.types = UMessageAuthorizationOptionBadge|UMessageAuthorizationOptionAlert;
+    [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+    [UMessage registerForRemoteNotificationsWithLaunchOptions:launchOptions Entity:entity completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (granted) {
+            // 用户选择了接收Push消息
+        }else{
+            // 用户拒绝接收Push消息
+        }
+    }];
+}
+
+-(void)initShareSdk{
+
+    [ShareSDK registerActivePlatforms:@[
+                                        @(SSDKPlatformTypeSinaWeibo),
+                                        @(SSDKPlatformTypeMail),
+                                        @(SSDKPlatformTypeCopy),
+                                        @(SSDKPlatformTypeWechat),
+                                        @(SSDKPlatformTypeQQ)                                        ]
+                             onImport:^(SSDKPlatformType platformType)
+     {
+         switch (platformType)
+         {
+             case SSDKPlatformTypeWechat:
+                 [ShareSDKConnector connectWeChat:[WXApi class]];
+                 break;
+             case SSDKPlatformTypeQQ:
+                 [ShareSDKConnector connectQQ:[QQApiInterface class] tencentOAuthClass:[TencentOAuth class]];
+                 break;
+             case SSDKPlatformTypeSinaWeibo:
+                 [ShareSDKConnector connectWeibo:[WeiboSDK class]];
+                 break;
+             default:
+                 break;
+         }
+     }
+                      onConfiguration:^(SSDKPlatformType platformType, NSMutableDictionary *appInfo)
+     {
+         
+         switch (platformType)
+         {
+             case SSDKPlatformTypeSinaWeibo:
+                 //设置新浪微博应用信息,其中authType设置为使用SSO＋Web形式授权
+                 [appInfo SSDKSetupSinaWeiboByAppKey:@"3780891600"
+                                           appSecret:@"9dd622fbca8d2ce9b90ce7fe63f4c16c"
+                                         redirectUri:@"http://www.abigfish.org"
+                                            authType:SSDKAuthTypeBoth];
+                 break;
+             case SSDKPlatformTypeWechat:
+                 [appInfo SSDKSetupWeChatByAppId:@"wx4868b35061f87885"
+                                       appSecret:@"64020361b8ec4c99936c0e3999a9f249"];
+                 break;
+             case SSDKPlatformTypeQQ:
+                 [appInfo SSDKSetupQQByAppId:@"1106573647"
+                                      appKey:@"PkUjxVUWwIX7cUiS"
+                                    authType:SSDKAuthTypeBoth];
+                 break;
+             default:
+                 break;
+         }
+     }];
+}
+
+
+-(void)setupXHLaunchAd{
+    
+    /** 1.图片开屏广告 - 网络数据 */
+    //[self example01];
+    
+    //2.******图片开屏广告 - 本地数据******
+    [self example02];
+    
+    //3.******视频开屏广告 - 网络数据(网络视频只支持缓存OK后下次显示,看效果请二次运行)******
+    //[self example03];
+    
+    /** 4.视频开屏广告 - 本地数据 */
+    //[self example04];
+    
+    /** 5.如需自定义跳过按钮,请看这个示例 */
+    //[self example05];
+    
+    /** 6.使用默认配置快速初始化,请看下面两个示例 */
+    //[self example06];//图片
+    //[self example07];//视频
+    
+    /** 7.如果你想提前批量缓存图片/视频请看下面两个示例 */
+    //[self batchDownloadImageAndCache]; //批量下载并缓存图片
+    //[self batchDownloadVideoAndCache]; //批量下载并缓存视频
+    
+}
+
+-(void)example02{
+    
+    //设置你工程的启动页使用的是:LaunchImage 还是 LaunchScreen.storyboard(不设置默认:LaunchImage)
+    [XHLaunchAd setLaunchSourceType:SourceTypeLaunchImage];
+    
+    //配置广告数据
+    XHLaunchImageAdConfiguration *imageAdconfiguration = [XHLaunchImageAdConfiguration new];
+    //广告停留时间
+    imageAdconfiguration.duration = 5;
+    //广告frame
+    imageAdconfiguration.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height * 0.8);
+    //广告图片URLString/或本地图片名(.jpg/.gif请带上后缀)
+    imageAdconfiguration.imageNameOrURLString = @"image12.gif";
+    //设置GIF动图是否只循环播放一次(仅对动图设置有效)
+    imageAdconfiguration.GIFImageCycleOnce = NO;
+    //图片填充模式
+    imageAdconfiguration.contentMode = UIViewContentModeScaleAspectFill;
+    //广告点击打开页面参数(openModel可为NSString,模型,字典等任意类型)
+    imageAdconfiguration.openModel = @"http://www.it7090.com";
+    //广告显示完成动画
+    imageAdconfiguration.showFinishAnimate =ShowFinishAnimateFlipFromLeft;
+    //广告显示完成动画时间
+    imageAdconfiguration.showFinishAnimateTime = 0.8;
+    //跳过按钮类型
+    imageAdconfiguration.skipButtonType = SkipTypeRoundProgressText;
+    //后台返回时,是否显示广告
+    imageAdconfiguration.showEnterForeground = NO;
+    //设置要添加的子视图(可选)
+    //imageAdconfiguration.subViews = [self launchAdSubViews];
+    //显示开屏广告
+    [XHLaunchAd imageAdWithImageAdConfiguration:imageAdconfiguration delegate:self];
+    
+}
 
 @end
