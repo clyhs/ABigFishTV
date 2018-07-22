390898475196ae2ff54eaeca06a6873455a44cc8
clyhs <clygd@126.com>
2018-07-22 11:05:50 +0800

    sharesdkicon appicon

--- ABigFishTV/Classes/Video/Controller/ABFChatViewController.m
+++ ABigFishTV/Classes/Video/Controller/ABFChatViewController.m
@@ -354,36 +354,35 @@
         
         NSMutableDictionary *shareParams = [NSMutableDictionary dictionary];
         [shareParams SSDKSetupShareParamsByText:@"分享内容"
-                                         images:@[@"http://mob.com/Assets/images/logo.png?v=20150320"]
-                                            url:[NSURL URLWithString:@"http://mob.com"]
-                                          title:@"分享标题"
-                                           type:SSDKContentTypeAuto];
+              images:@[@"http://mob.com"]
+              url:[NSURL URLWithString:@"http://mob.com"]
+              title:@"分享标题"
+        type:SSDKContentTypeAuto];
         //有的平台要客户端分享需要加此方法，例如微博
         [shareParams SSDKEnableUseClientShare];
         //2、分享（可以弹出我们的分享菜单和编辑界面）
-        [ShareSDK showShareActionSheet:nil //要显示菜单的视图, iPad版中此参数作为弹出菜单的参照视图，只有传这个才可以弹出我们的分享菜单，可以传分享的按钮对象或者自己创建小的view 对象，iPhone可以传nil不会影响
+        [ShareSDK showShareActionSheet:nil
                                  items:nil
                            shareParams:shareParams
                    onShareStateChanged:^(SSDKResponseState state, SSDKPlatformType platformType, NSDictionary *userData, SSDKContentEntity *contentEntity, NSError *error, BOOL end) {
-                       
                        switch (state) {
                            case SSDKResponseStateSuccess:
                            {
                                UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"分享成功"
-                                                                                   message:nil
-                                                                                  delegate:nil
-                                                                         cancelButtonTitle:@"确定"
-                                                                         otherButtonTitles:nil];
+                                   message:nil
+                                   delegate:nil
+                                   cancelButtonTitle:@"确定"
+                                otherButtonTitles:nil];
                                [alertView show];
                                break;
                            }
                            case SSDKResponseStateFail:
                            {
                                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"分享失败"
-                                                                               message:[NSString stringWithFormat:@"%@",error]
-                                                                              delegate:nil
-                                                                     cancelButtonTitle:@"OK"
-                                                                     otherButtonTitles:nil, nil];
+                                   message:[NSString stringWithFormat:@"%@",error]
+                                   delegate:nil
+                                   cancelButtonTitle:@"OK"
+                                   otherButtonTitles:nil, nil];
                                [alert show];
                                break;
                            }
